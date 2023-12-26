db70358f1923188047c9da665493207e94259f18
Mohammed Seid <mdseid111@gmail.com>
2023-12-26 10:56:46 +0300

    added the function sellWeapon

--- script.js
+++ script.js
@@ -42,6 +42,23 @@ const locations = [
         text: "You enter the cave. You see some monsters."
     }
 ];
+const monsters=[
+  {
+    name : "slime",
+    level: 2,
+    health: 15
+  },
+  {
+    name : "fanged beast",
+    level: 8,
+    health: 60
+  },
+  {
+    name : "dragon",
+    level: 20,
+    health: 300
+  }
+  ]
 
 // initialize buttons
 button1.onclick = goStore;
@@ -85,13 +102,33 @@ function buyHealth() {
   }
 }
 function buyWeapon() {
-  if (gold >= 30) {
-    gold -= 30;
-    currentWeapon++;
+  if (currentWeapon < weapons.length - 1) {
+    if (gold >= 30) {
+      gold -= 30;
+      currentWeapon++;
+      goldText.innerText = gold;
+      let newWeapon = weapons[currentWeapon].name;
+      text.innerText = "You now have a " + newWeapon + ".";
+      inventory.push(newWeapon);
+      text.innerText += " In your inventory you have: " + inventory;
+    } else {
+      text.innerText = "You do not have enough gold to buy a weapon.";
+    }
+  } else {
+    text.innerText = "You already have the most powerful weapon!";
+    button2.innerText= "Sell weapon for 15 gold";
+    button2.onclick=sellWeapon;
+  }
+}
+function sellWeapon() {
+  if (inventory.length > 1) {
+    gold += 15;
     goldText.innerText = gold;
-    let newWeapon = weapons[currentWeapon].name;
-    text.innerText = "You now have a " + newWeapon + ".";
-    
+    let currentWeapon = inventory.shift();
+    text.innerText = "You sold a " + currentWeapon + ".";
+    text.innerText += " In your inventory you have: " + inventory;
+  } else{
+    text.innerText = "Don't sell your only weapon!"
   }
 }
 function fightSlime() {
